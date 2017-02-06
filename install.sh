9745f2576b46dbc20536ada47cf42c4f4e2f00b9
pax <coolwinding@gmail.com>
2017-02-06 15:59:42 +0800

    Some improvement

--- install.sh
+++ install.sh
@@ -3,6 +3,5 @@
 P=`dirname $(readlink -f $0)`
 
 mkdir -p ~/bin
-ln -fs $P/xbackup.sh ~/bin/xbackup.sh
-ln -fs $P/xrestore.sh ~/bin/xrestore.sh
-
+ln -fs $P/xbackup ~/bin/xbackup
+ln -fs $P/xrestore ~/bin/xrestore
